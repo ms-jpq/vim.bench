ae477d4cf3840f898632419c65a87c2b016ffaa9
Fly me to the moon <git@bigly.dog>
2021-09-24 13:15:12 -0700

    stats

--- fs/code/benchmark/stats.py
+++ fs/code/benchmark/stats.py
@@ -1,35 +1,23 @@
 from pathlib import PurePath
 from statistics import fmean, stdev
-from typing import Iterable, Iterator, Mapping, Sequence, Tuple
+from typing import Sequence
 
 from seaborn import kdeplot
+from std2.statistics import quantiles
 
 from .types import Stats
 
 
-def _quantiles(data: Iterable[float], *quantiles: int) -> Mapping[int, float]:
-    ordered = sorted(data)
-
-    def cont() -> Iterator[Tuple[int, float]]:
-        for quantile in quantiles:
-            assert quantile >= 0 and quantile <= 100
-            if ordered:
-                idx = round((len(ordered) - 1) * quantile)
-                yield quantile, ordered[idx]
-
-    return {k: v for k, v in cont()}
-
-
 def stats(sample: Sequence[float]) -> Stats:
     mean, std = fmean(sample), stdev(sample)
-    quantiles = _quantiles(sample, 0, 50, 95, 100)
+    quads = quantiles(sample, 0, 50, 95, 100)
     stats = Stats(
         mean=mean,
         std=std,
-        q0=quantiles[0],
-        q50=quantiles[50],
-        q95=quantiles[95],
-        q100=quantiles[100],
+        q0=quads[0],
+        q50=quads[50],
+        q95=quads[95],
+        q100=quads[100],
     )
     return stats
 
