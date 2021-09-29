5ea25305accc820f165b9151b2ed01e0674e5714
Fly me to the moon <git@bigly.dog>
2021-09-29 05:07:15 -0700

    ok

--- fs/root/.config/nvim/autoload/bench.vim
+++
@@ -1,3 +0,0 @@
-function! bench#comp_nvim() abort
-  autocmd InsertEnter * lua require('completion').on_attach()
-endfunction
--- fs/root/.config/nvim/lua/tst_comp_nvim.lua
+++ fs/root/.config/nvim/lua/tst_comp_nvim.lua
@@ -1,8 +1,5 @@
-vim.g.completion_chain_complete_list = {
-  default = {
-    {complete_items = {"lsp"}},
-    {complete_items = {"buffers"}},
-    {complete_items = {"path"}}
-  }
-}
-vim.fn["bench#comp_nvim"]()
+vim.schedule(
+  function()
+    require("completion").on_attach()
+  end
+)
